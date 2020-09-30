8966bee600a9d97daf218b5d5c7ffb07f6fc1441
MarijnOomensAvans <38461006+MarijnOomensAvans@users.noreply.github.com>
2020-09-30 20:22:33 +0200

    dll in het mappie

--- Delta-dungeons/Delta-dungeons/Delta-dungeons.cpp
+++ Delta-dungeons/Delta-dungeons/Delta-dungeons.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include "SDL.h";
 
-int main()
+int main(int argc, char* argv[])
 {
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window* window = SDL_CreateWindow("ccccronk", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
