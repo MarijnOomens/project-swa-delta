9956487208e4a03de925e265a63f2cf6e1da6bff
Robin van den Udenhout <rlmudenh@avans.nl>
2020-10-27 11:46:43 +0100

    fix so that release will also build

--- Delta-dungeons/Engine/main.h
+++ Delta-dungeons/Engine/main.h
@@ -5,6 +5,12 @@
 #else 
 #define ENGINE_API __declspec(dllimport) 
 #endif
+#elif NDEBUG
+#ifdef ENGINE_EXPORTS
+#define ENGINE_API __declspec(dllexport)
+#else 
+#define ENGINE_API __declspec(dllimport) 
+#endif
 #else
 #define ENGINE_API
 #endif
