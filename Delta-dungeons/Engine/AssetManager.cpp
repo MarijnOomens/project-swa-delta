15de4ba93f0ed8c0a407320548bf2a99b4cd9d27
Yoran de Haan <32639287+yymar@users.noreply.github.com>
2020-10-27 13:32:52 +0100

    Fix Small Error

--- Delta-dungeons/Engine/AssetManager.cpp
+++ Delta-dungeons/Engine/AssetManager.cpp
@@ -13,6 +13,4 @@ std::string AssetManager::getTexture(std::string id)
 	return textures[id];
 }
 
-std::string AssetManager::getFont(std::string id) {}
-
 void AssetManager::addFont(std::string id, std::string path, int fontSize) {}
