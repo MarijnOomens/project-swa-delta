2744f379494b45505000e59b8cc0a2f8ac01ab29
Marijn <oomensmarijn@gmail.com>
2020-10-28 11:12:52 +0100

    Button shrink fix

--- Delta-dungeons/Delta-dungeons/Button.cpp
+++ Delta-dungeons/Delta-dungeons/Button.cpp
@@ -18,20 +18,7 @@ Button::~Button() {}
 
 void Button::update() {}
 
-void Button::handleInput(const KeyCodes keyCode, const KeyboardEvent keyboardEvent)
-{
-	std::cout << keyCode << std::endl;
-	if (keyCode == KEY_UP) {
-		this->transform.position.x + 1;
-	}
-	else if (keyCode == KEY_DOWN) {
-		this->transform.position.x - 1;
-	}
-	for (auto& comp : components)
-	{
-		comp->transform = this->transform;
-	}
-};
+void Button::handleInput(const KeyCodes keyCode, const KeyboardEvent keyboardEvent) {};
 
 void Button::connectCallback() {}
 
