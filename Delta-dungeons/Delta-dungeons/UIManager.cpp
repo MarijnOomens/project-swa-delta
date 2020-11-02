0bf5da5339313921bc1f992937ff417a989e51ea
Yoran de Haan <32639287+yymar@users.noreply.github.com>
2020-11-02 15:21:02 +0100

    Add documentation

--- Delta-dungeons/Delta-dungeons/UIManager.cpp
+++ Delta-dungeons/Delta-dungeons/UIManager.cpp
@@ -1,30 +1,20 @@
 #include "UIManager.h"
 
 void UIManager::playDialogue(std::vector<std::string>) {}
+
 void UIManager::createBaseScreens() {
 	//std::shared_ptr<MainMenu> mainMenu = std::make_shared<MainMenu>();
 	//screens.try_emplace("MainMenu", mainMenu);
 
 	std::shared_ptr<CreditScreen> credits = std::make_shared<CreditScreen>();
 	screens.try_emplace("Credits", credits);
-
-
 }
 
-void UIManager::updateHudHealth(int)
-{
+void UIManager::updateHudHealth(int) {}
 
-}
+void UIManager::updateHudCollectedCrystals(int) {}
 
-void UIManager::updateHudCollectedCrystals(int)
-{
-
-}
-
-void UIManager::updateHighScore(int)
-{
-
-}
+void UIManager::updateHighScore(int) {}
 
 std::map<std::string, std::string> UIManager::passTextures()
 {
