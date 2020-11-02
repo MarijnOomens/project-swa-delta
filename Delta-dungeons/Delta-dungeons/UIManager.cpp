f17f192eeac8473e362007f570825fdf69a3c7fc
Ymke <ymke-noorlander@hotmail.nl>
2020-11-02 15:14:13 +0100

    Update UIManager.cpp

--- Delta-dungeons/Delta-dungeons/UIManager.cpp
+++ Delta-dungeons/Delta-dungeons/UIManager.cpp
@@ -1,13 +1,16 @@
 #include "UIManager.h"
 
 void UIManager::playDialogue(std::vector<std::string>) {}
+
+/// <summary>
+/// This methods creates all screens needed for the game to run. Examples are the "MainMenu" and "Credits" screens. These are added into a list.
+/// </summary>
 void UIManager::createBaseScreens() {
 	//std::shared_ptr<MainMenu> mainMenu = std::make_shared<MainMenu>();
 	//screens.try_emplace("MainMenu", mainMenu);
 
 	std::shared_ptr<CreditScreen> credits = std::make_shared<CreditScreen>();
 	screens.try_emplace("Credits", credits);
-
 
 }
 
@@ -26,6 +29,10 @@ void UIManager::updateHighScore(int)
 
 }
 
+/// <summary>
+/// passTextures gets all the textures saved in the screens and gives them back in one big list for the GameManager.
+/// </summary>
+/// <returns>A map of all textures inside the screens.</returns>
 std::map<std::string, std::string> UIManager::passTextures()
 {
 	std::map<std::string, std::string> totalTextures;
@@ -37,6 +44,10 @@ std::map<std::string, std::string> UIManager::passTextures()
 	return totalTextures;
 }
 
+/// <summary>
+/// passFonts gets all the fonts saved in the screens and gives them back in one big list for the GameManager.
+/// </summary>
+/// <returns>A map of all fonts inside the screens.</returns>
 std::map<std::string, std::string> UIManager::passFonts()
 {
 	std::map<std::string, std::string> totalFonts;
