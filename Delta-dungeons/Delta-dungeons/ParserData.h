c2243236f28815af482c20690347bf3ed839b2db
Marijn <oomensmarijn@gmail.com>
2020-11-11 16:22:50 +0100

    Game XMLParser refactor

--- Delta-dungeons/Delta-dungeons/ParserData.h
+++ Delta-dungeons/Delta-dungeons/ParserData.h
@@ -4,9 +4,9 @@
 class ParserData {
 
 public:
-	ParserData();
-	~ParserData();
+	ParserData() {};
+	~ParserData() {};
 
-	ParserData(const std::string x, const std::string y, const std::string tileId);
+	ParserData(const std::string& x, const std::string& y, const std::string& tileId);
 	std::string x, y, tileId;
 };
