9680ab6e83423f40a05e3377814c5ee3e4fbafba
tommeren <t.vanommeren@student.avans.nl>
2020-11-20 16:59:05 +0100

    Added hint on game over

--- Delta-dungeons/Delta-dungeons/HelpScreen.cpp
+++ Delta-dungeons/Delta-dungeons/HelpScreen.cpp
@@ -18,27 +18,31 @@ HelpScreen::HelpScreen()
 	this->components.emplace_back(std::move(helpText));
 
 	std::unique_ptr<TextComponent> helpMove = std::make_unique<TextComponent>("Use arrow keys or W, A, S and D to move your character.", "joystix", color, 25);
-	helpMove->transform.position = { 50, 220 };
+	helpMove->transform.position = { 50, 120 };
 	this->components.emplace_back(std::move(helpMove));
 
 	std::unique_ptr<TextComponent> helpInteract = std::make_unique<TextComponent>("Use E to interact.", "joystix", color, 25);
-	helpInteract->transform.position = { 50, 300 };
+	helpInteract->transform.position = { 50, 200 };
 	this->components.emplace_back(std::move(helpInteract));
 
 	std::unique_ptr<TextComponent> helpBack = std::make_unique<TextComponent>("Press BACKSPACE to go back to the main menu.", "joystix", color, 25);
-	helpBack->transform.position = { 50, 380 };
+	helpBack->transform.position = { 50, 280 };
 	this->components.emplace_back(std::move(helpBack));
 
 	std::unique_ptr<TextComponent> helpFps = std::make_unique<TextComponent>("Press TAB to toggle the FPS counter in the upper right corner in-game.", "joystix", color, 25);
-	helpFps->transform.position = { 50, 460 };
+	helpFps->transform.position = { 50, 360 };
 	this->components.emplace_back(std::move(helpFps));
 
 	std::unique_ptr<TextComponent> helpGameSpeed = std::make_unique<TextComponent>("Press , to decrease FPS. Press . to increase FPS. Press / to reset FPS to 60.", "joystix", color, 25);
-	helpGameSpeed->transform.position = { 50, 560 };
+	helpGameSpeed->transform.position = { 50, 440 };
 	this->components.emplace_back(std::move(helpGameSpeed));
 
+	std::unique_ptr<TextComponent> helpGameOver = std::make_unique<TextComponent>("Game over is triggered when the player is out of hearts. This can be done by walking into any Pokémon.", "joystix", color, 25);
+	helpGameOver->transform.position = { 50, 520 };
+	this->components.emplace_back(std::move(helpGameOver));
+
 	std::vector<std::string> possibleButtonTexExit = { "button_exit" };
-	std::unique_ptr<Button> exitButton = std::make_unique<Button>(500, 720, possibleButtonTexExit, staticBackCallbackFunction, this);
+	std::unique_ptr<Button> exitButton = std::make_unique<Button>(500, 620, possibleButtonTexExit, staticBackCallbackFunction, this);
 	this->components.emplace_back(std::move(exitButton));
 }
 
