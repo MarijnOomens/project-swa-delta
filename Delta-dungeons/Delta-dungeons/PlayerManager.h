b83c16457cc1de590d6bf04d6176a59d173a3b16
Ymke <ymke-noorlander@hotmail.nl>
2020-11-30 22:38:17 +0100

    fixes

--- Delta-dungeons/Delta-dungeons/PlayerManager.h
+++ Delta-dungeons/Delta-dungeons/PlayerManager.h
@@ -8,7 +8,7 @@ typedef void(*cbCamera) (void*, int, int);
 typedef void(*cbInteract) (void*, std::shared_ptr<BehaviourObject>, int, int);
 typedef void(*cbGameOver) (void*);
 typedef void(*cbHUD) (void*, int, int, int);
-typedef void(*cbCollision) (void*, std::shared_ptr<BehaviourObject>, int, int, KeyCodes);
+typedef void(*cbCollision) (void*, std::shared_ptr<BehaviourObject>, int, int, KeyCodes, int);
 typedef void(*cbNextLevel) (void*);
 
 class PlayerManager
