e7dd4615ecaa55300e8713331310087e88de7d5b
Robin van den Udenhout <rlmudenh@avans.nl>
2020-12-03 16:45:45 +0100

    fix issue 71

--- Delta-dungeons/Delta-dungeons/SceneLoader.h
+++ Delta-dungeons/Delta-dungeons/SceneLoader.h
@@ -16,11 +16,13 @@ public:
 	void addOverlayScene(const std::string& sceneName) const;
 	void setEngineFacade(std::shared_ptr<EngineFacade> engineFacade);
 	void quitGame() const;
+	void setCurrentLevel(std::string levelName);
+	std::string getCurrentLevel();
 
 private:
 	static SceneLoader sceneLoader;
 	std::shared_ptr<EngineFacade> engineFacade;
-
+	std::string currentLevel;
 	SceneLoader() {}
 };
 
