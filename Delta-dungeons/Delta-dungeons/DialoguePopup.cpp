0674629a4413cde5278826193ac1780ae9d1db84
Robin van den Udenhout <rlmudenh@avans.nl>
2020-12-04 12:11:55 +0100

    better box texture

--- Delta-dungeons/Delta-dungeons/DialoguePopup.cpp
+++ Delta-dungeons/Delta-dungeons/DialoguePopup.cpp
@@ -5,9 +5,9 @@ DialoguePopup::DialoguePopup()
 	gc = std::make_unique<GraphicsComponent>();
 	gc->setTexture("dialogue_box");
 	gc->isScreen = true;
-	gc->imageDimensions = { 840, 292 };
+	gc->imageDimensions = { 1280, 292 };
 
-	this->transform.position = { 100, 500 };
+	this->transform.position = { 5, 700 };
 	gc->transform.position = this->transform.position;
 
 	this->components.emplace_back(std::move(gc));
