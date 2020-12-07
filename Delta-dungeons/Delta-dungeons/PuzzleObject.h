c782fd7902aa10fce288a1a67ab933bda8f7f0e4
Raymond <rjhho@avans.nl>
2020-12-07 19:37:31 +0100

    add logic notes in puzzleobject

--- Delta-dungeons/Delta-dungeons/PuzzleObject.h
+++ Delta-dungeons/Delta-dungeons/PuzzleObject.h
@@ -5,6 +5,15 @@
 
 class PuzzleObject : IInteractiveObject
 {
+
+	//interact add the direction
+		//directions decides if either x or y gets adjusted
+		//call checkcollsion(cc);
+	//if hasmoved = true
+		// dont move
+	//else 
+		//call move method within puzzleobject
+		//call func method of puzzle.cpp
 protected:
 	std::string sfxPath;
 };
