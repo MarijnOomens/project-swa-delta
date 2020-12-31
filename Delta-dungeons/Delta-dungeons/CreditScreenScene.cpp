fe05d39cce92261133563a6d8137ea6389c01fbb
MarijnOomensAvans <38461006+MarijnOomensAvans@users.noreply.github.com>
2020-12-31 14:20:42 +0100

    more music + volume changes

--- Delta-dungeons/Delta-dungeons/CreditScreenScene.cpp
+++ Delta-dungeons/Delta-dungeons/CreditScreenScene.cpp
@@ -6,5 +6,5 @@ CreditScreenScene::CreditScreenScene()
 	this->setBehaviourObjects({ std::make_shared<CreditScreen>() });
 	this->setTexture("credits", "Assets/Menu/Credits/background.png");
 	this->setTexture("button_back", "Assets/Menu/Button/button-back.png");
-	this->setBeat("credits", "Assets/Audio/credits.wav");
+	this->setBeat("mii", "Assets/Audio/mii.ogg");
 }
--- Delta-dungeons/Delta-dungeons/Creditscreen.h
+++ Delta-dungeons/Delta-dungeons/Creditscreen.h
@@ -4,6 +4,7 @@
 #include "Button.h"
 #include "TextComponent.h"
 #include "SceneLoader.h"
+#include "AudioUtilities.h"
 
 class CreditScreen : public Screen 
 {
@@ -11,6 +12,8 @@ public:
 	CreditScreen();
 
 	void handleInput(const KeyCodes& keyCode, const KeyboardEvent& keyboardEvent, Vector2D& mousePos) override;
+
+	void start() override;
 
 	static void staticBackCallbackFunction(const void* p);
 	void backCallbackFunction() const;
