c5ef5e259ba9adcc05e7e47a92b8f439f74416c6
Robin van den Udenhout <rlmudenh@avans.nl>
2021-01-05 10:45:18 +0100

    disabele bug

--- Delta-dungeons/Engine/InputWrapper.cpp
+++ Delta-dungeons/Engine/InputWrapper.cpp
@@ -93,7 +93,7 @@ void InputWrapper::handleKeyPressed(const bool isGamePaused, const bool isInputP
 			break;
 			}
 		}
-	else if (event.key.keysym.sym == SDLK_ESCAPE) 
+	else if (event.key.keysym.sym == SDLK_ESCAPE && !isInputPaused) 
 	{
 		func(pointer, KeyCodes::KEY_ESC, KeyboardEvent::KEY_PRESSED, mousePosition);
 	}
